7e3d24194881d82c060e92d1af424e7f65686825
snmishra <satya.devel@gmail.com>
2022-03-18 18:22:24 +0000

    Bump up version and requirements

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ from setuptools import find_packages
 
 setup(
     name='vcf-dedup',
-    version='0.4.9',
+    version='0.5.0',
     description='Removal of duplicated variants from a VCF',
     packages=find_packages(),
     scripts=['scripts/vcf_dedupper'],
-    url='https://github.com/genomicsengland/vcf-dedup',
-    download_url='https://github.com/genomicsengland/vcf-dedup/archive/v0.4.8.tar.gz',
+    url='https://github.com/snmishra/vcf-dedup',
     license='Apache',
     author='Pablo Riesgo Ferreiro',
-    author_email='pablo.riesgo-ferreiro@genomicsengland.co.uk',
-    requires=['PyVCF', 'argparse'],
-    install_requires=['PyVCF==0.6.8'],
+    author_email='satya.devel@gmail.com',
+    requires=['PyVCF3'],
+    install_requires=['PyVCF3==1.0.3'],
     keywords=['VCF']
 )
